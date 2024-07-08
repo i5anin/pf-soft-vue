aafc871a57f522630d127799ecb55b05454cdcf1
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-07-08 15:25:30 +0300

    Update UseCoefficient.sql

--- backend/src/controllers/tool/sql/UseCoefficient.sql
+++ backend/src/controllers/tool/sql/UseCoefficient.sql
@@ -1,21 +1,32 @@
 WITH ToolUsage
-         AS (SELECT CASE WHEN tn.group_standard THEN tn.group_id ELSE tn.id END AS tool_group_id, -- Группируем по group_id, если инструмент эталонный
-                    SUM(vl.new_amount)                                          AS total_taken,
-                    COUNT(*)                                                    AS usage_count
-             FROM dbo.vue_log vl
-                      JOIN dbo.tool_nom tn ON vl.tool_id = tn.id
-             GROUP BY tool_group_id),
-     TotalTaken AS (SELECT SUM(new_amount) AS total_taken_all FROM dbo.vue_log)
-SELECT tn.id                                                             AS tool_id,
-       tn.name                                                           AS tool_name,
-       tn.group_id,
-       tn.group_standard,
-       COALESCE(tu.usage_count, 0)                                       AS usage_count,
-       COALESCE(tu.total_taken, 0)                                       AS total_taken,
-       tt.total_taken_all,
-       (COALESCE(tu.total_taken, 0)::DECIMAL / tt.total_taken_all) * 100 AS taken_coefficient
-FROM dbo.tool_nom tn
-         LEFT JOIN ToolUsage tu ON CASE WHEN tn.group_standard THEN tn.group_id ELSE tn.id END =
-                                   tu.tool_group_id -- Объединяем по group_id, если инструмент эталонный
-         CROSS JOIN TotalTaken tt
+         AS
+         (SELECT CASE
+                     WHEN dbo.tool_nom.group_standard
+                         THEN dbo.tool_nom.group_id
+                     ELSE dbo.tool_nom.id END AS tool_group_id,
+                 SUM(vue_log.new_amount)      AS total_taken,
+                 COUNT(*)                     AS usage_count
+          FROM dbo.vue_log
+                   JOIN dbo.tool_nom
+                        ON vue_log.tool_id = tool_nom.id
+          GROUP BY tool_group_id),
+     TotalTaken AS (SELECT SUM(vue_log.new_amount)
+                               AS total_taken_all
+                    FROM dbo.vue_log)
+SELECT tool_nom.id                                                                                AS tool_id,
+       tool_nom.name                                                                              AS tool_name,
+       tool_nom.group_id,
+       tool_nom.group_standard,
+       COALESCE(ToolUsage.usage_count, 0)                                                         AS usage_count,
+       COALESCE(ToolUsage.total_taken, 0)                                                         AS total_taken,
+       TotalTaken.total_taken_all,
+       ROUND((COALESCE(ToolUsage.total_taken, 0)::DECIMAL / TotalTaken.total_taken_all) * 100, 4) AS taken_coefficient
+FROM dbo.tool_nom
+         LEFT JOIN ToolUsage ON
+    CASE
+        WHEN tool_nom.group_standard
+            THEN tool_nom.group_id
+        ELSE tool_nom.id
+        END = ToolUsage.tool_group_id
+         CROSS JOIN TotalTaken
 ORDER BY taken_coefficient DESC;
