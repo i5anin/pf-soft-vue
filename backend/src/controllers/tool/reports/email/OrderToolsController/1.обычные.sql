dce90b8ffd6e5922dfecbf73aef1606945bd31d2
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-07-09 11:42:31 +0300

    +

--- backend/src/controllers/tool/reports/email/OrderToolsController/1.обычные.sql
+++ backend/src/controllers/tool/reports/email/OrderToolsController/1.обычные.sql
@@ -4,13 +4,8 @@ SELECT tool_nom.id                     AS id_tool,
        -- заказ
        tool_nom.norma - tool_nom.sklad AS zakaz,
        tool_nom.sklad,
-       tool_nom.norma,
-       -- группа
-       tool_nom.group_id,
-       tool_nom.group_standard,
-       -- 3 нормы
-       tool_nom.norma_green,
-       tool_nom.norma_red
+       tool_nom.norma
+
 FROM dbo.tool_nom
 WHERE tool_nom.norma - tool_nom.sklad > 0
   AND (group_id = 0 OR group_id isnull);
