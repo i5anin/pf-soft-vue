347a3c0ca1ece2817b6b23276065727869440529
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-07-09 16:55:04 +0300

    sql
    Update OrderToolsController.js
    
    Update 5_general.sql
    
    +
    
    ref
    
    Revert "Update OrderToolsController.js"
    
    This reverts commit fbe952bc28a31161840c7fa05ff0f81d0de75651.
    
    Update 4_group_3_norma.sql

--- backend/src/controllers/tool/reports/email/OrderToolsController/4_group_3_norma.sql
+++ backend/src/controllers/tool/reports/email/OrderToolsController/4_group_3_norma.sql
@@ -1,11 +1,10 @@
--- 4.группы 3 нормы
 SELECT tool_nom.id AS id_tool,
        tool_nom.name,
        CASE
            WHEN tool_nom.group_id <> 0
                THEN tool_nom.norma - group_totals.group_sklad
            ELSE tool_nom.norma_green - tool_nom.sklad
-           END AS zakaz,
+           END     AS zakaz,
        tool_nom.sklad,
        tool_nom.norma,
        tool_nom.norma_green,
@@ -14,19 +13,18 @@ SELECT tool_nom.id AS id_tool,
        tool_nom.group_standard,
        group_totals.group_sklad
 FROM dbo.tool_nom
-LEFT JOIN (
-    SELECT group_id, SUM(sklad) AS group_sklad
-    FROM dbo.tool_nom
-    GROUP BY group_id
-) AS group_totals ON tool_nom.group_id = group_totals.group_id
+         LEFT JOIN (SELECT group_id, SUM(sklad) AS group_sklad
+                    FROM dbo.tool_nom
+                    GROUP BY group_id) AS group_totals ON tool_nom.group_id = group_totals.group_id
 WHERE (tool_nom.norma - tool_nom.sklad > 0 OR tool_nom.norma_green - tool_nom.sklad > 0)
   AND (
     (tool_nom.group_id <> 0 AND (tool_nom.norma - group_totals.group_sklad) > 0) OR
     (tool_nom.group_id = 0 AND (tool_nom.norma_green > 0) AND (tool_nom.norma_red > 0))
-  )
+    )
   AND (CASE
            WHEN tool_nom.group_id <> 0
                THEN tool_nom.norma - group_totals.group_sklad
            ELSE tool_nom.norma_green - tool_nom.sklad
-       END) > 0
-  AND tool_nom.group_standard = 'true'; -- Добавлено условие group_standard = true 
+    END) > 0
+  AND tool_nom.group_standard = 'true'
+  AND tool_nom.norma_green IS NOT NULL;
