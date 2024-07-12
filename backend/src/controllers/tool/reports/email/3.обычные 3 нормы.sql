bcb1d003127b1e10c44eedf7aa0e78778f118e19
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-07-12 11:23:39 +0300

    old

---
+++ backend/src/controllers/tool/reports/email/3.обычные 3 нормы.sql
@@ -0,0 +1,15 @@
+-- заказ обычный инструмент "3 нормы" "нет группы"
+SELECT tool_nom.id                     AS id_tool,
+       tool_nom.name,
+       -- заказ
+       tool_nom.norma_green - tool_nom.sklad AS zakaz,
+       tool_nom.sklad,
+       tool_nom.norma,
+       -- 3 нормы
+       tool_nom.norma_green,
+       tool_nom.norma_red
+FROM dbo.tool_nom
+WHERE tool_nom.norma - tool_nom.sklad > 0
+  AND (tool_nom.norma_green <> 0)
+  AND (tool_nom.norma_red <> 0)
+  AND (group_id = 0 OR group_id isnull);
---
+++ backend/src/controllers/tool/reports/email/4.группы 3 нормы.sql
@@ -0,0 +1,31 @@
+SELECT tool_nom.id AS id_tool,
+       tool_nom.name,
+       CASE
+           WHEN tool_nom.group_id <> 0
+               THEN tool_nom.norma - group_totals.group_sklad
+           ELSE tool_nom.norma_green - tool_nom.sklad
+           END AS zakaz,
+       tool_nom.sklad,
+       tool_nom.norma,
+       tool_nom.norma_green,
+       tool_nom.norma_red,
+       tool_nom.group_id,
+       tool_nom.group_standard,
+       group_totals.group_sklad
+FROM dbo.tool_nom
+LEFT JOIN (
+    SELECT group_id, SUM(sklad) AS group_sklad
+    FROM dbo.tool_nom
+    GROUP BY group_id
+) AS group_totals ON tool_nom.group_id = group_totals.group_id
+WHERE (tool_nom.norma - tool_nom.sklad > 0 OR tool_nom.norma_green - tool_nom.sklad > 0)
+  AND (
+    (tool_nom.group_id <> 0 AND (tool_nom.norma - group_totals.group_sklad) > 0) OR
+    (tool_nom.group_id = 0 AND (tool_nom.norma_green > 0) AND (tool_nom.norma_red > 0))
+  )
+  AND (CASE
+           WHEN tool_nom.group_id <> 0
+               THEN tool_nom.norma - group_totals.group_sklad
+           ELSE tool_nom.norma_green - tool_nom.sklad
+       END) > 0
+  AND tool_nom.group_standard = 'true'; -- Добавлено условие group_standard = true
