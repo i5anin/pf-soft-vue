bd6067e13fd4dfd529df89494ca36c03b8a31900
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-07-17 16:23:20 +0300

    sql

---
+++ backend/src/controllers/tool/my-sql-file/graf/Graf(Общее среднее выданного инструмента за всё время).sql
@@ -0,0 +1,7 @@
+SELECT
+    DATE(datetime_log) AS date,
+    SUM(old_amount - new_amount) AS total_kolvo
+FROM dbo.vue_log
+WHERE  old_amount - new_amount > 0
+GROUP BY DATE(datetime_log)
+ORDER BY date
