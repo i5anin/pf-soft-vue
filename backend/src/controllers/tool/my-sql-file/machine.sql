4b83cab0ff64b92d8b8be06dc58e875a7965847f
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-07-19 15:11:27 +0300

    Update machine.sql

--- backend/src/controllers/tool/my-sql-file/machine.sql
+++ backend/src/controllers/tool/my-sql-file/machine.sql
@@ -1,13 +1,12 @@
 SELECT CASE
-           WHEN id = 999 THEN CAST(id AS VARCHAR) || ':' || cnc_name
-           ELSE CAST(id AS VARCHAR) || ':' || cnc_name
+           WHEN id = 27 THEN id::VARCHAR || ':' || cnc_name
+           ELSE id::VARCHAR  || ':' || cnc_name
            END AS Combined_Data
-FROM (SELECT 999 AS id, 'Другое оборудование' AS cnc_name -- "Другое оборудование" first
+FROM (SELECT 27 AS id, 'Другое оборудование' AS cnc_name -- "Другое оборудование" first
       UNION ALL
       (SELECT id, cnc_name
        FROM dbo.cnc
        WHERE plan = TRUE
          AND active = TRUE
        ORDER BY CASE WHEN cnc_name = 'Другое оборудование' THEN 0 ELSE 1 END, id) -- Order other rows
-     ) AS CombinedData
-ORDER BY id;
+     ) AS CombinedData;
