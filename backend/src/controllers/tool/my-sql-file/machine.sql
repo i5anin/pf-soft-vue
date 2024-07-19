8e31ac906c78779538569b6c891040a82cfbee0c
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-07-19 15:05:11 +0300

    Update machine.sql

--- backend/src/controllers/tool/my-sql-file/machine.sql
+++ backend/src/controllers/tool/my-sql-file/machine.sql
@@ -1,9 +1,13 @@
-SELECT 999, 'Другое оборудование'
-
-UNION ALL  -- Use UNION ALL to avoid duplicate removal if '999' exists in your table
-
-(SELECT id, cnc_name
- FROM dbo.cnc
- WHERE plan = TRUE
-   AND active = TRUE
- ORDER BY id)  -- Maintain order within the table data
+SELECT CASE
+           WHEN id = 999 THEN CAST(id AS VARCHAR) || ':' || cnc_name
+           ELSE CAST(id AS VARCHAR) || ':' || cnc_name
+           END AS Combined_Data
+FROM (SELECT 999 AS id, 'Другое оборудование' AS cnc_name -- "Другое оборудование" first
+      UNION ALL
+      (SELECT id, cnc_name
+       FROM dbo.cnc
+       WHERE plan = TRUE
+         AND active = TRUE
+       ORDER BY CASE WHEN cnc_name = 'Другое оборудование' THEN 0 ELSE 1 END, id) -- Order other rows
+     ) AS CombinedData
+ORDER BY id;
