2fa664524c6393cd0642e38ac5b513a082a6d7fe
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-07-29 11:26:52 +0300

    Update piniaStore.js

--- src/modules/tools/editor/piniaStore.js
+++ src/modules/tools/editor/piniaStore.js
@@ -1,8 +1,7 @@
-// src/stores/tool.js
 import { defineStore } from 'pinia'
 import { toolApi } from '@/api'
 
-export const useToolStore = defineStore('tool', {
+export const useEditorToolStore = defineStore('editorToolStore', {
   state: () => ({
     isLoading: false,
     parentCatalog: { id: 1, label: null },
@@ -11,27 +10,16 @@ export const useToolStore = defineStore('tool', {
     tool: null,
     tools: [],
     toolsTotalCount: 0,
-  }),
-  getters: {
-    formattedTools(state) {
-      return state.tools.map((tool) => ({
-        ...tool,
-        ...Object.entries(tool.property).reduce(
-          (acc, [key, { value }]) => ({
-            ...acc,
-            [key]: value,
-          }),
-          {}
-        ),
-      }))
+    filters: {
+      currentPage: 1,
+      itemsPerPage: 15,
+      search: '',
+      includeNull: false,
+      onlyInStock: null,
+      selectedDynamicFilters: {},
     },
-  },
+  }),
   actions: {
-    /**
-     * Получает информацию об инструменте по его ID.
-     * @param {number} id - ID инструмента.
-     * @returns {Promise<void>}
-     */
     async fetchToolById(id) {
       try {
         this.tool = await toolApi.getToolById(id)
@@ -40,10 +28,6 @@ export const useToolStore = defineStore('tool', {
       }
     },
 
-    /**
-     * Получает динамические фильтры для инструментов на основе выбранного родительского каталога.
-     * @returns {Promise<void>}
-     */
     async fetchToolsDynamicFilters() {
       const { id = null } = this.parentCatalog
       if (id === null) {
@@ -52,75 +36,129 @@ export const useToolStore = defineStore('tool', {
 
       try {
         const dynamicFilters = await toolApi.filterParamsByParentId(id)
+        this.filters.selectedDynamicFilters = dynamicFilters.reduce(
+          (acc, { key }) => ({ ...acc, [key]: null }),
+          {}
+        )
         this.dynamicFilters = dynamicFilters
-
-        this.selectedDynamicFilters = {
-          ...this.selectedDynamicFilters,
-          ...dynamicFilters.reduce(
-            (acc, { key }) => ({ ...acc, [key]: null }),
-            {}
-          ),
-        }
       } catch (e) {
         console.error('Ошибка при загрузке динамических фильтров:', e)
       }
     },
 
-    /**
-     * Получает список инструментов с учетом фильтров.
-     * @param {Object} params - Параметры фильтрации.
-     * @param {number} params.currentPage - Текущая страница.
-     * @param {number} params.itemsPerPage - Количество элементов на странице.
-     * @param {string} params.search - Строка поиска.
-     * @param {boolean} params.includeNull - Включать ли элементы с пустыми значениями.
-     * @param {boolean} [params.onlyInStock] - Показывать ли только товары в наличии.
-     * @param {Object} params.selectedDynamicFilters - Выбранные значения динамических фильтров.
-     * @param {number} params.parentId - ID родительского каталога.
-     * @returns {Promise<void>}
-     */
-    async fetchToolsByFilter(params) {
+    async fetchToolsByFilter() {
       this.isLoading = true
 
-      const {
-        currentPage,
-        itemsPerPage,
-        search,
-        includeNull,
-        onlyInStock = null,
-        selectedDynamicFilters,
-        parentId = this.parentCatalog.id,
-      } = params
-
-      // Формируем параметры запроса
-      const queryParams = new URLSearchParams({
-        page: currentPage.toString(),
-        limit: itemsPerPage.toString(),
-        includeNull: includeNull.toString(),
-        onlyInStock: onlyInStock?.toString(),
-        search,
-      })
-
-      if (parentId !== null)
-        queryParams.append('parent_id', parentId.toString())
-
-      // Добавляем динамические фильтры
-      Object.entries(selectedDynamicFilters).forEach(([key, value]) => {
-        if (value !== null) {
-          queryParams.append(`param_${key}`, value)
-        }
-      })
-
       try {
-        const { tools, totalCount } = await toolApi.getTools(
-          queryParams.toString()
-        )
+        const {
+          currentPage,
+          itemsPerPage,
+          search,
+          includeNull,
+          onlyInStock,
+          selectedDynamicFilters,
+        } = this.filters
+        const { id: parentId } = this.parentCatalog
+
+        const { tools, totalCount } = await toolApi.getTools({
+          search,
+          page: currentPage,
+          limit: itemsPerPage,
+          includeNull,
+          parentId,
+          onlyInStock,
+          ...Object.entries(selectedDynamicFilters).reduce(
+            (acc, [key, value]) => ({ ...acc, [`param_${key}`]: value }),
+            {}
+          ),
+        })
+
         this.tools = tools
         this.toolsTotalCount = totalCount
       } catch (error) {
-        console.error('Ошибка при получении данных:', error)
+        console.error('getTools. Ошибка при получении данных:', error)
       } finally {
         this.isLoading = false
       }
     },
+
+    setSearch(search) {
+      this.filters.search = search
+    },
+
+    setParentCatalog(parentCatalog) {
+      this.parentCatalog = { ...parentCatalog }
+    },
+
+    setDynamicFilters(dynamicFilters) {
+      this.dynamicFilters = dynamicFilters
+    },
+
+    setSelectedDynamicFilters(selectedDynamicFilters) {
+      this.filters.selectedDynamicFilters = selectedDynamicFilters
+    },
+
+    setIsLoading(isLoading) {
+      this.isLoading = isLoading
+    },
+
+    setCurrentPage(page) {
+      this.filters.currentPage = page
+    },
+
+    setFilters(filters) {
+      this.filters = { ...filters }
+    },
+
+    setTool(tool) {
+      this.tool = tool
+    },
+
+    setItemsPerPage(itemsPerPage) {
+      this.filters.itemsPerPage = itemsPerPage
+    },
+
+    setToolsTotalCount(toolTotalCount) {
+      this.toolsTotalCount = toolTotalCount
+    },
+
+    setTools(tools) {
+      this.tools = tools
+    },
+
+    setOnlyInStock(onlyInStock) {
+      this.filters.onlyInStock = onlyInStock
+    },
+  },
+  getters: {
+    parentCatalog: (state) => state.parentCatalog,
+    dynamicFilters: (state) => state.dynamicFilters,
+    filters: (state) => ({ ...state.filters }),
+    tool: (state) => {
+      if (state.tool) {
+        return {
+          ...state.tool,
+          property: state.tool.property,
+          parent_id: state.tool.parent_id,
+          folder_name: state.tool.folder_name,
+        }
+      }
+      return null
+    },
+    tools: (state) => [...state.tools],
+    formattedTools: (state) =>
+      state.tools.map((tool) => ({
+        ...tool,
+        ...Object.entries(tool.property).reduce(
+          (acc, [key, { value }]) => ({
+            ...acc,
+            [key]: value,
+          }),
+          {}
+        ),
+      })),
+    isLoading: (state) => state.isLoading,
+    nameOptions: (state) => state.nameOptions,
+    toolsTotalCount: (state) => state.toolsTotalCount,
   },
 })
