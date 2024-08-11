3e4b1c7841619ddf8473b4cf320fd90507ce49d7
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-08-11 18:34:37 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'backend soft',
-      script: 'nodemon',
-      args: 'backend/src/app.js',
-      watch: true,
+      script: './node_modules/.bin/nodemon backend/src/app.js', // Используйте локальный nodemon
+      watch: false, // Nodemon сам следит за изменениями
       env: {
         PM2_PUBLIC_KEY: process.env.PM2_PUBLIC_KEY,
         PM2_SECRET_KEY: process.env.PM2_SECRET_KEY,
@@ -15,11 +14,10 @@ module.exports = {
     },
     {
       name: 'frontend soft',
-      script: 'npx',
-      args: 'vite --host',
-      interpreter: 'none',
+      script: 'npx vite --host', // Укажите команду для npx
+      interpreter: 'none', // Не нужно интерпретировать команду
       instances: 1,
-      watch: true,
+      watch: false, // Не нужно следить за изменениями, vite сам это делает
       env: {
         PM2_PUBLIC_KEY: process.env.PM2_PUBLIC_KEY,
         PM2_SECRET_KEY: process.env.PM2_SECRET_KEY,
