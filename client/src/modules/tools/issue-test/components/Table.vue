385534c5576ecdd05658a1d44fa1589f54b416c5
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-08-26 10:16:27 +0300

    Update Table.vue

--- client/src/modules/tools/issue-test/components/Table.vue
+++ client/src/modules/tools/issue-test/components/Table.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
-    <div class="text-right">
-      <v-btn color="blue" @click="onAddTool">
+    <div class='text-right'>
+      <v-btn color='blue' @click='onAddTool'>
         <template #prepend>
           <v-icon>mdi-file-plus</v-icon>
         </template>
@@ -9,76 +9,93 @@
       </v-btn>
     </div>
     <editor-tool-modal
-      v-if="openDialog"
-      :persistent="true"
-      :tool-id="editingToolId"
-      @canceled="onClosePopup"
-      @changes-saved="handleSaveChanges"
+      v-if='openDialog'
+      :persistent='true'
+      :tool-id='editingToolId'
+      @canceled='onClosePopup'
+      @changes-saved='handleSaveChanges'
     />
     <v-data-table-server
-      v-if="isDataLoaded"
-      no-data-text="Нет данных"
-      items-per-page-text="Пункты на странице:"
-      loading-text="Загрузка данных"
-      :headers="toolTableHeaders"
-      :items="formattedTools"
-      :items-length="totalToolsCount"
-      :items-per-page="itemsPerPage"
-      :page="currentPage"
-      :loading="isLoading"
-      :items-per-page-options="[15, 50, 100, 300]"
-      density="compact"
-      class="elevation-1 scrollable-table"
+      v-if='isDataLoaded'
+      no-data-text='Нет данных'
+      items-per-page-text='Пункты на странице:'
+      loading-text='Загрузка данных'
+      :headers='toolTableHeaders'
+      :items='formattedTools'
+      :items-length='totalToolsCount'
+      :items-per-page='itemsPerPage'
+      :page='currentPage'
+      :loading='isLoading'
+      :items-per-page-options='[15, 50, 100, 300]'
+      density='compact'
+      class='elevation-1 scrollable-table'
       hover
       fixed-header
       width
-      @update:page="handlePageChange"
-      @update:items-per-page="handleItemsPerPageChange"
-      @click:row="handleRowClick"
+      @update:page='handlePageChange'
+      @update:items-per-page='handleItemsPerPageChange'
+      @click:row='handleRowClick'
     >
-      <template #item.index="{ index }">
-        <td class="index">
+      <template #item.index='{ index }'>
+        <td class='index'>
           {{ calculateItemIndex(index) }}
         </td>
       </template>
-      <template #item.name="{ item }">
-        <td style="white-space: nowrap">
-          <span :class="{ grey: !item.sklad || item.sklad === 0 }">
+      <template #item.name='{ item }'>
+        <td style='white-space: nowrap'>
+          <span :class='{ grey: !item.sklad || item.sklad === 0 }'>
             {{ item.name }}
           </span>
           <v-chip
-            v-if="item.group_id"
-            size="x-small"
-            :color="getColorForGroup(item.group_id)"
+            v-if='item.group_id'
+            size='x-small'
+            :color='getColorForGroup(item.group_id)'
             :title="'Группа ' + item.group_id"
           >
-            <span v-if="item.group_standard" style="color: yellow">★</span>
+            <span v-if='item.group_standard' style='color: yellow'>★</span>
             G{{ item.group_id }}
           </v-chip>
         </td>
       </template>
-      <template #item.sklad="{ item }">
-        <td style="white-space: nowrap">
-          <v-chip :color="item.sklad === 0 ? 'red' : ''">
+      <template #item.sklad='{ item }'>
+        <td style='white-space: nowrap'>
+          <v-chip :color="item.sklad === 0 ? 'red' : appColor()">
             {{ item.sklad }}
           </v-chip>
+          <span v-if='item.norma' class='grey'> / {{ item.norma }} </span>
         </td>
       </template>
-      <template #item.norma="{ item }">
-        <td style="white-space: nowrap">
-          <span class="green" v-if="item.norma_green">
+      <template #item.norma='{ item }'>
+        <td style='white-space: nowrap'>
+          <span class='green' v-if='item.norma_green'>
             {{ item.norma_green }}
-            <span class="grey">|</span>
+            <span class='grey'>|</span>
           </span>
           {{ item.norma }}
-          <span class="red" v-if="item.norma_red">
-            <span class="grey">|</span>
+          <span class='red' v-if='item.norma_red'>
+            <span class='grey'>|</span>
             {{ item.norma_red }}
           </span>
         </td>
       </template>
-      <template #item.zakaz="{ item }">
-        <td style="white-space: nowrap">{{ calculateToolOrder(item) }}</td>
+      <template #item.zakaz='{ item }'>
+        <td style='white-space: nowrap'>{{ calculateToolOrder(item) }}</td>
+      </template>
+      <template #item.damaged='{ item }'>
+        <td align='center'>
+          <v-btn color='red' size='small' icon='mdi-image-broken-variant'
+                 >
+<!--            @click='(event) => onDamagedTool(event, item)'-->
+          </v-btn>
+        </td>
+      </template>
+      <template #item.cart='{ item }'>
+        <td align='center'>
+          <v-btn color='yellow' size='small' icon='mdi-cart-arrow-down'
+                 >
+<!--            @click='addToolToCart(item.id, 1)'-->
+          </v-btn>
+        </td>
       </template>
     </v-data-table-server>
   </v-container>
@@ -88,7 +105,7 @@
 import EditorToolModal from './modal/ModalCart.vue'
 import ToolFilter from './filter/Filter.vue'
 import { useEditorToolStore } from '../piniaStore'
-import { getColorForGroup } from '@/utils/colorUtils'
+import { appColor, getColorForGroup } from '@/utils/colorUtils'
 
 export default {
   components: {
@@ -154,28 +171,21 @@ export default {
           { title: 'Норма', key: 'norma', sortable: false },
           ...(dynamicColumns && dynamicColumns.length > 0
             ? dynamicColumns.map(({ label: title, key }) => ({
-                title,
-                key,
-                sortable: false,
-              }))
+              title,
+              key,
+              sortable: false,
+            }))
             : []),
         ]
       },
     },
-    // 'currentItem.id': {
-    //   handler(newId, oldId) {
-    //     if (newId !== oldId) {
-    //       // Проверяем, изменился ли id
-    //       this.fetchToolsData()
-    //     }
-    //   },
-    // },
   },
   mounted() {
     this.fetchToolsData()
     this.isDataLoaded = true
   },
   methods: {
+    appColor,
     getColorForGroup,
     handlePageChange(page) {
       this.editorToolStore.setCurrentPage(page)
@@ -190,7 +200,7 @@ export default {
         index +
         1 +
         (this.editorToolStore.getFilters.currentPage - 1) *
-          this.editorToolStore.getFilters.itemsPerPage
+        this.editorToolStore.getFilters.itemsPerPage
       )
     },
     calculateToolOrder(tool) {
@@ -221,20 +231,22 @@ export default {
     },
   },
 }
-</script>
+</template>
 
 <style scoped>
-.index {
-  max-width: 40px !important;
+  .index {
+  max - width: 40px !important;
   font-size: 0.9em;
   color: grey;
 }
 
-.red {
+  .red {
   color: red;
 }
 
-.green {
+  .green {
   color: green;
 }
 </style>
+<script setup>
+</script>
