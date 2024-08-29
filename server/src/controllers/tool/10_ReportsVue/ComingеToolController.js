8635f51f11a2d953c1c261ca96c301cdc764d11e
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-08-29 16:17:23 +0300

    Update ComingеToolController.js
    Revert "Update ComingеToolController.js"
    
    This reverts commit 855469137c248f17bc409b1027cb256e673c6419.
    
    Update ComingеToolController.js

--- server/src/controllers/tool/10_ReportsVue/ComingеToolController.js
+++ server/src/controllers/tool/10_ReportsVue/ComingеToolController.js
@@ -4,59 +4,78 @@ const getDbConfig = require('../../../config/databaseConfig')
 const dbConfig = getDbConfig()
 const pool = new Pool(dbConfig)
 
-async function getComingTool(req, res) {
-  try {
-    const page = parseInt(req.query.page || 1, 10)
-    const limit = parseInt(req.query.limit || 15, 10)
-    const offset = (page - 1) * limit
-    const date = req.query.date
+const selectComingToolQuery = `
+  SELECT vue_log.*,
+         vue_users.login AS user_name,
+         tool_nom.name   AS tool_name
+  FROM dbo.vue_log
+           LEFT JOIN dbo.vue_users ON vue_log.user_id = vue_users.id
+           LEFT JOIN dbo.tool_nom ON vue_log.tool_id = tool_nom.id
+`
 
-    let whereClause = 'WHERE (message LIKE \'%Добавлен%\' OR (message LIKE \'%Обновлен%\' AND (old_amount IS NULL OR new_amount > old_amount)))'
+const getComingToolWhereClause = (date) => {
+  let whereClause = 'WHERE (message LIKE \'%Добавлен%\' OR (message LIKE \'%Обновлен%\' AND (old_amount IS NULL OR new_amount > old_amount)))'
+  if (date) {
+    whereClause += ` AND CAST(datetime_log AS DATE) = '${date}'`
+  }
+  return whereClause
+}
 
-    if (date) {
-      whereClause += ` AND CAST(datetime_log AS DATE) = '${date}'`
-    }
+const getComingTools = async (page, limit, date) => {
+  const offset = (page - 1) * limit
+  const whereClause = getComingToolWhereClause(date)
 
-    const query = `
-        SELECT vue_log.*,
-               vue_users.login AS user_name,
-               tool_nom.name   AS tool_name
-        FROM dbo.vue_log
-                 LEFT JOIN dbo.vue_users ON vue_log.user_id = vue_users.id
-                 LEFT JOIN dbo.tool_nom ON vue_log.tool_id = tool_nom.id
-            ${whereClause}
-        ORDER BY datetime_log DESC
-        LIMIT ${limit} OFFSET ${offset};
-    `
+  const query = `${selectComingToolQuery}
+                 ${whereClause}
+  ORDER BY datetime_log DESC
+  LIMIT ${limit} OFFSET ${offset};
+  `
+  const result = await pool.query(query)
+  return result.rows.map((tool) => ({
+    ...tool,
+    datetime_log: tool.datetime_log.toISOString(),
+  }))
+}
 
-    const countQuery = `
+const getTotalCount = async (date) => {
+  const whereClause = getComingToolWhereClause(date)
+  const countQuery = `
         SELECT COUNT(*) AS total_count
         FROM dbo.vue_log
                  LEFT JOIN dbo.vue_users ON vue_log.user_id = vue_users.id
                  LEFT JOIN dbo.tool_nom ON vue_log.tool_id = tool_nom.id
             ${whereClause}
     `
+  const countResult = await pool.query(countQuery)
+  return parseInt(countResult.rows[0].total_count, 10)
+}
 
-    const result = await pool.query(query)
-    const countResult = await pool.query(countQuery)
-    const comingTools = result.rows
-    const totalCount = parseInt(countResult.rows[0].total_count, 10)
+async function getComingTool(req, res) {
+  try {
+    const page = parseInt(req.query.page || 1, 10)
+    const limit = parseInt(req.query.limit || 15, 10)
+    const date = req.query.date
 
-    const formattedComingTools = comingTools.map((tool) => ({
-      ...tool,
-      datetime_log: tool.datetime_log.toISOString(),
-    }))
+    const comingTools = await getComingTools(page, limit, date)
+    const totalCount = await getTotalCount(date)
 
     res.json({
       currentPage: page,
       itemsPerPage: limit,
       totalCount: totalCount,
-      comingTools: formattedComingTools,
+      comingTools: comingTools,
     })
   } catch (error) {
     console.error('Ошибка при получении данных:', error)
     res.status(500).send('Ошибка при получении данных')
   }
+}
+
+const getFormattedDate = (date) => {
+  const year = date.getFullYear()
+  const month = ('0' + (date.getMonth() + 1)).slice(-2)
+  const day = ('0' + date.getDate()).slice(-2)
+  return `${year}-${month}-${day}`
 }
 
 async function getComingToolDates(req, res) {
@@ -68,13 +87,8 @@ async function getComingToolDates(req, res) {
                    ORDER BY date DESC`
     const result = await pool.query(query)
 
-    const dates = result.rows.map(row => {
-      const year = row.date.getFullYear()
-      const month = ('0' + (row.date.getMonth() + 1)).slice(-2) // Месяцы начинаются с 0
-      const day = ('0' + row.date.getDate()).slice(-2)
-      return `${year}-${month}-${day}`
-    })
-    
+    const dates = result.rows.map(row => getFormattedDate(row.date))
+
     res.json(dates)
   } catch (error) {
     console.error('Ошибка при получении данных:', error)
