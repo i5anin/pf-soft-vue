00741f1b4a046f0903eff4f0b9673706fed21036
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-09-05 16:39:00 +0300

    client node -> Apache

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,19 +12,19 @@ module.exports = {
       instances: 1,
       exec_mode: 'cluster',
     },
-    {
-      name: 'client',
-      cwd: './client',
-      script: 'npx',
-      args: 'vite --host',
-      interpreter: 'none',
-      instances: 1,
-      watch: true,
-      env: {
-        PM2_PUBLIC_KEY: process.env.PM2_PUBLIC_KEY,
-        PM2_SECRET_KEY: process.env.PM2_SECRET_KEY,
-        NODE_ENV: process.env.NODE_ENV,
-      },
-    },
+    // {
+    //   name: 'client',
+    //   cwd: './client',
+    //   script: 'npx',
+    //   args: 'vite --host',
+    //   interpreter: 'none',
+    //   instances: 1,
+    //   watch: true,
+    //   env: {
+    //     PM2_PUBLIC_KEY: process.env.PM2_PUBLIC_KEY,
+    //     PM2_SECRET_KEY: process.env.PM2_SECRET_KEY,
+    //     NODE_ENV: process.env.NODE_ENV,
+    //   },
+    // },
   ],
 }
